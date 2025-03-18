e368bd6fa419110dc334d4b1c5f90f754b7b6ad9
Adwoa Asare <robotics.mags@gmail.com>
2025-03-18 10:57:31 -0400

    added gitignore

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # retrofit-radiator
 Project for class 12770
+
+make a file called wifi_config,py and put the following lines
+
+'''python
+# Wi-Fi credentials (DO NOT SHARE THIS FILE)
+
+WIFI_SSID = "YourWiFiSSID"
+WIFI_PASSWORD = "YourWiFiPassword"
+'''
--- datastream.py
+++ datastream.py
@@ -1,8 +1,11 @@
 #functions to stream data from RPi pico W to our PC
 
+from wifi_config import WIFI_SSID, WIFI_PASSWORD
+
 def send_data_to_pc(data):
     """
     Sends data to the PC remotely.
     """
+    print(f"Connecting to Wi-Fi SSID: {WIFI_SSID}")
     print(f"Sending data to PC: {data}")
     # Add your data streaming logic here
